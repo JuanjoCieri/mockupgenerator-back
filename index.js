71dd774997b50ef4562fff79e3755097ca84b13b
JuanjoCieri <juin@hotmail.com.ar>
2023-07-21 16:25:42 -0300

    setHeaders

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ import { getDeviceFromFileName } from "./functions/getDeviceFromFileName.js";
 const app = express();
 const PORT = 3000;
 
+function setHeaders(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, token, session, cookie"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+  );
+  next();
+}
+
+app.use(setHeaders);
+
 app.use(
   cors({
     credentials: true,
